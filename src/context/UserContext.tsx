72ebf6f7aab5b6c435b974f47fc23cc06832cab9
Felipe Caroca <felipecaroca24@gmail.com>
2025-06-18 15:17:25 -0400

    Refactor: Se crea hook useUser.ts para centralizar acceso y actualizacion del usuario global

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-import type { UserContextProps } from '../types';
+import type { UserContextProps } from '../types/index';
 
 const UserContext = createContext<UserContextProps | undefined>(undefined);
 
